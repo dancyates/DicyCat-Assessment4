2224d6857d42c1b8194b7d78a2a56c77ea6cb41f
IsaacAlbiston <ia737@york.ac.uk>
2020-02-26 11:12:20 +0000

    Created PowerUp class

---
+++ core/src/com/dicycat/kroy/misc/PowerUp.java
@@ -0,0 +1,19 @@
+package com.dicycat.kroy.misc;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.Vector2;
+import com.dicycat.kroy.GameObject;
+
+public class PowerUp extends GameObject {
+    private String powerUpType;
+
+    public PowerUp(Vector2 spawnPos, Texture image, Vector2 imSize, String powerUpType) {
+        super(spawnPos, image, imSize);    //Constructor; takes the screen to be put on, spawn position vector, image and a vector for its size
+
+    }
+
+    public void update() {
+
+    }
+}
