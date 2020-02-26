88107bbc52e878ab0c161539c4593dc2b4f79099
Luke T <lnt511@york.ac.uk>
2020-02-26 11:13:51 +0000

    getter

--- core/src/com/dicycat/kroy/misc/PowerUp.java
+++ core/src/com/dicycat/kroy/misc/PowerUp.java
@@ -10,10 +10,15 @@ public class PowerUp extends GameObject {
 
     public PowerUp(Vector2 spawnPos, Texture image, Vector2 imSize, String powerUpType) {
         super(spawnPos, image, imSize);    //Constructor; takes the screen to be put on, spawn position vector, image and a vector for its size
+        this.powerUpType = powerUpType;
 
     }
 
     public void update() {
 
     }
+    
+    public String getPowerUpType() {
+		return powerUpType;
+	}
 }
