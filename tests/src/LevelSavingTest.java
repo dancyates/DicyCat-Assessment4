a5c8bfe1f1b0270bee0bc5743cd65b230680f395
mc1923 <mc1923@york.ac.uk>
2020-04-28 16:10:19 +0100

    edited a saving test

--- tests/src/LevelSavingTest.java
+++ tests/src/LevelSavingTest.java
@@ -27,9 +27,8 @@ public class LevelSavingTest {
 	public void fireTruckShouldSaveToTheCorrectPosition(){
 		//Emulates the saving of a game in the case that a medium difficulty game were saved into slot 0 at the point the game is initialised.
 		testGameSave.saveGame(0,1, new Vector2(234 * 16, 3900),60*15,6,0, new Vector2(0,0),0);
-		testGameSave.addPlayer(new FireTruck(new Vector2(234 * 16, 3900), new Float[]{0f,0f,0f}, 2));
 
-		assertEquals(new Vector2(234 * 16, 3900),testGameSave.getPlayers().get(0).getPosition());
+		assertEquals(new Vector2(234 * 16, 3900),testGameSave.getSpawnPos());
 	}
 
 	/**
